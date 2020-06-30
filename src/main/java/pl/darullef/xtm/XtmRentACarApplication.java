f58194a0e2addf8892ff9c88c6d8dda2e0006ed6
darullef <darullef4@gmail.com>
2020-06-30 21:27:30 +0200

    Initial commit

---
+++ src/main/java/pl/darullef/xtm/XtmRentACarApplication.java
@@ -0,0 +1,13 @@
+package pl.darullef.xtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtmRentACarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XtmRentACarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/darullef/xtm/XtmRentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.darullef.xtm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtmRentACarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
