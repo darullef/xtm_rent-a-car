b8f3b8926046a14134e369a6edb9b58db3e9b7d0
darullef <darullef4@gmail.com>
2020-07-02 11:33:22 +0200

    Code cleanup

--- src/main/java/pl/darullef/xtm/Controller/RentController.java
+++ src/main/java/pl/darullef/xtm/Controller/RentController.java
@@ -14,6 +14,7 @@ import pl.darullef.xtm.Service.RentService;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
@@ -24,39 +25,46 @@ public class RentController {
 
     @Autowired
     private RentService rentService;
+    private final JSONParser parser = new JSONParser();
 
-    @PostMapping
-    public ResponseEntity<?> addRent(HttpEntity<String> httpEntity) throws org.json.simple.parser.ParseException {
-        JSONParser parser = new JSONParser();
+    List<Date> parsedDate(HttpEntity<String> httpEntity) throws org.json.simple.parser.ParseException {
         JSONObject json = (JSONObject) parser.parse(httpEntity.getBody());
-        Date rent_start, rent_end;
-        UUID client_id, car_id;
-
+        List<Date> dates = new ArrayList<>();
         try {
             java.util.Date rent_start_temp = new SimpleDateFormat("dd/MM/yyyy").parse(json.get("rent_start").toString());
             java.util.Date rent_end_temp = new SimpleDateFormat("dd/MM/yyyy").parse(json.get("rent_end").toString());
-            rent_start = new Date(rent_start_temp.getTime());
-            rent_end = new Date(rent_end_temp.getTime());
+            dates.add(new Date(rent_start_temp.getTime()));
+            dates.add(new Date(rent_end_temp.getTime()));
         } catch (ParseException ex) {
             throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, "Start & end date required"
+                    HttpStatus.BAD_REQUEST
             );
         }
+        return dates;
+    }
 
+    List<UUID> parsedUUID(HttpEntity<String> httpEntity) throws org.json.simple.parser.ParseException {
+        JSONObject json = (JSONObject) parser.parse(httpEntity.getBody());
+        List<UUID> uuids = new ArrayList<>();
         try {
-            client_id = UUID.fromString(json.get("client_id").toString());
-            car_id = UUID.fromString(json.get("car_id").toString());
+            uuids.add(UUID.fromString(json.get("client_id").toString()));
+            uuids.add(UUID.fromString(json.get("car_id").toString()));
         } catch (IllegalArgumentException | NoSuchElementException ex) {
             throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, "Bad UUID format"
+                    HttpStatus.BAD_REQUEST
             );
         }
-        // TODO: NoSuchElementException nie jest łapany, gdy uuid jest za krótkie
+        return uuids;
+    }
 
+    @PostMapping
+    public ResponseEntity<?> addRent(HttpEntity<String> httpEntity) throws org.json.simple.parser.ParseException {
+        List<Date> dates = parsedDate(httpEntity);
+        List<UUID> uuids = parsedUUID(httpEntity);
         try {
-            Rent rent = rentService.createRent(rent_start, rent_end, client_id, car_id);
+            Rent rent = rentService.createRent(dates.get(0), dates.get(1), uuids.get(0), uuids.get(1));
             return new ResponseEntity<>(rent, HttpStatus.CREATED);
-        } catch (NullPointerException ex) {
+        } catch (NullPointerException | NoSuchElementException ex) {
             throw new ResponseStatusException(
                     HttpStatus.CONFLICT, "Can not create a rent. Car is taken in this period of time"
             );
@@ -89,34 +97,11 @@ public class RentController {
 
     @PutMapping(path = "/{uuid}")
     public ResponseEntity<?> updateRent(@PathVariable("uuid") UUID uuid, HttpEntity<String> httpEntity) throws org.json.simple.parser.ParseException {
-        JSONParser parser = new JSONParser();
-        JSONObject json = (JSONObject) parser.parse(httpEntity.getBody());
-        Date rent_start, rent_end;
-        UUID client_id, car_id;
-
-        try {
-            java.util.Date rent_start_temp = new SimpleDateFormat("dd/MM/yyyy").parse(json.get("rent_start").toString());
-            java.util.Date rent_end_temp = new SimpleDateFormat("dd/MM/yyyy").parse(json.get("rent_end").toString());
-            rent_start = new Date(rent_start_temp.getTime());
-            rent_end = new Date(rent_end_temp.getTime());
-        } catch (ParseException ex) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, "Start & end date required"
-            );
-        }
-
-        try {
-            client_id = UUID.fromString(json.get("client_id").toString());
-            car_id = UUID.fromString(json.get("car_id").toString());
-        } catch (IllegalArgumentException | NoSuchElementException ex) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, "Bad UUID format"
-            );
-        }
-
+        List<Date> dates = parsedDate(httpEntity);
+        List<UUID> uuids = parsedUUID(httpEntity);
         try {
             Rent oldRent = rentService.getRentById(uuid);
-            rentService.updateRent(oldRent, rent_start, rent_end, client_id, car_id);
+            rentService.updateRent(oldRent, dates.get(0), dates.get(1), uuids.get(0), uuids.get(1));
             return new ResponseEntity<>(oldRent, HttpStatus.OK);
         } catch (NoSuchElementException ex) {
             throw new ResponseStatusException(
